03be7f4b1104b5a71320ab939882ec55812ada15
AdrianC64 <adriacol64@gmail.com>
2021-12-22 17:04:57 +0100

    il file è identico a prima, solo che ho eliminato le parti di programma commentate che contenevano le vecchie implementazioni, così da rendere il codice più leggibile

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -20,13 +20,6 @@ the_uri_parse(SchemeL,
               URIList,
               []),
 atom_chars_wrapper(Scheme, SchemeL),
-/*analisi_uri(Scheme,
-            UserinfoL,
-            HostL,
-            PortL,
-            PathL,
-            QueryL,
-            FragmentL),*/
 atom_chars_wrapper(Userinfo, UserinfoL),
 atom_chars_wrapper(Host, HostL),
 number_codes(Port, PortL), %serve per avere come porta un numero e non un atomo
@@ -71,79 +64,8 @@ write('Fragment: '),
 writeln(Fragment),
 close(Stream).
 
-%regola uri_analisis/7 che controlla che i "token" della grammatica
-% delle uri riconosciuti dal parser corrispondano alla sintassi
-% corrispondente a seconda del contenuto del campo "Scheme"
-/*
-analisi_uri(Scheme,
-            _Userinfo,
-            _Host,
-            [],
-            [],
-            [],
-            []) :-
-!,
-Scheme = 'mailto'.
-
-analisi_uri(Scheme,
-            _Userinfo,
-            [],
-            [],
-            [],
-            [],
-            []) :-
-!,
-Scheme = 'tel'.
-
-analisi_uri(Scheme,
-            _Userinfo,
-            [],
-            [],
-            [],
-            [],
-            []) :-
-!,
-Scheme = 'fax'.
-
-analisi_uri(Scheme,
-            _Userinfo,
-            _Host,
-            _Port,
-            Path,
-            _Query,
-            _Fragment) :-
-!,
-Scheme = 'zos',
-check_path(Path, []).
-
-analisi_uri(_, _, _, _, _, _, _) :- !.
-*/
-/*implementazione sbagliata, che si appoggia ad analisi_uri
-check_path --> [C], {controllo_alfanum(C)}, id44(1).
-check_path --> [C], {controllo_alfanum(C)}, id44(1),
-['(', B], {controllo_alfanum(B)}, id8(1), [')'].
-
-id44(L) --> [C], {controllo_alfanum(C), L =< 43}.
-id44(L) --> [C], {controllo_alfanum(C), L2 is L + 1}, id44(L2).
-id44(L) --> ['.'], {L2 is L + 1}, id44(L2).
-
-id8(L) --> [C], {controllo_alfanum(C), L =< 7}.
-id8(L) --> [C], {controllo_alfanum(C), L2 is L + 1}, id8(L2).
-*/
-/* old implementation
-id44_recursive([H | []]) --> [H], {controllo_alfanum(H)}.
-id44_recursive([H | T]) --> [H], {controllo_alfanum(H)}, id44_recursive(T).
-id44_recursive(['.' | T]) --> ['.'], id44_recursive(T).
-*/
-/* old implementation of id8
-id8([H | T]) --> [H], {controllo_alfa(H)},
-id8_recursive(T).
-
-id8_recursive([H | []]) --> [H], {controllo_alfanum(H)}.
-id8_recursive([H | T]) --> [H], {controllo_alfanum(H)}, id8_recursive(T).
-*/
-
-% Regole per il parsing
+% Regole per il parsing, controllo del campo Scheme e selezione della
+% corretta grammatica
 
 the_uri_zos_parse(Userinfo,
                   Host,
@@ -161,12 +83,13 @@ the_uri_parse(Scheme,
               Path,
               Query,
               Fragment) -->
-{string_chars("zos", Scheme)}, Scheme,{!}, [':'], the_uri_zos_parse(Userinfo,
-                                                    Host,
-                                                    Port,
-                                                    Path,
-                                                    Query,
-                                                    Fragment).
+{string_chars("zos", Scheme)}, Scheme,{!}, [':'],
+the_uri_zos_parse(Userinfo,
+                  Host,
+                  Port,
+                  Path,
+                  Query,
+                  Fragment).
 
 the_uri_parse(Scheme,
               Userinfo,
@@ -208,57 +131,10 @@ scheme(Scheme),
 authorithy(Userinfo, Host, Port),
 path_query_frag(Path, Query, Fragment).
 
-%regole generali
-
-
-/* sembra non servire più
-the_uri_parse(Scheme,
-              [],
-              [],
-              [],
-              Path,
-              Query,
-              Fragment) -->
-scheme(Scheme),
-[':'],
-opt_slash_path_quer_frag(Path, Query, Fragment).
-*/
-% da implementare: le specifiche aggiuntive del pdf
-
-% sintassi_speciali(Userinfo,
-%             Host,
-%             Port,
-%             Path,
-%             Query,
-%             Fragment).
-% sintassi speciali
-
-% sintassi_speciali(Userinfo,
-%             Host,
-%             [],
-%             [],
-%             [],
-%             []) -->
-% mailto(Userinfo, Host).
-
-% sintassi_speciali([],
-%             Host,
-%             [],
-%             [],
-%             [],
-%             []) -->
-% news(Host).
-
-% sintassi_speciali(Userinfo,
-%             Host,
-%             Port,
-%             Path,
-%             Query,
-%             Fragment) -->
-% authorithy(Userinfo, Host, Port),
-% path_quer_frag(Path, Query, Fragment).
+% fine regole parsing generali
 
 % implementazione delle sotto-grammatiche
+
 scheme(Scheme) --> identificatore2(Scheme).
 
 authorithy(Userinfo, Host, Port) --> ['/'], ['/'],
@@ -269,6 +145,7 @@ userinfo([]) --> [].
 userinfo(Userinfo) --> identificatore2(Userinfo), ['@'].
 
 % host da implementare
+
 host(Host) --> identificatore_host2(H), host_opt(T),
 {append(H, T, Host)}.
 host(Host) --> indirizzo_ip(Host).
@@ -280,41 +157,15 @@ host_opt(['.' | Host_opt]) --> ['.'], identificatore_host2(H), host_opt(T),
 port(['8', '0']) --> [].
 port(Port) --> [':'], digit2(Port).
 
-/* capire se vogliamo usarlo ancora
-slash_path_quer_frag([],
-                     [],
-                     []) -->
-[].
-slash_path_quer_frag(Path,
-                     Query,
-                     Fragment) -->
-['/'],
-path_quer_frag(Path, Query, Fragment).
-*/
-/* sembra non sia più necessario
-opt_slash_path_quer_frag(Path,
-                         Query,
-                         Fragment) -->
-slash,
-path_quer_frag(Path, Query, Fragment).
-
-slash --> [].
-slash --> ['/'].
-*/
 path_query_frag(Path, Query, Fragment) -->
 ['/'], path(Path), query(Query), fragment(Fragment).
 path_query_frag([], [], []) --> [].
-
 
 % sintassi speciale zos
 
 zos_path_query_frag(Path, Query, Fragment) -->
 ['/'], zos_path(Path), query(Query), fragment(Fragment).
 zos_path_query_frag([], [], []) --> [].
-
-
-% path_quer_frag(Path, Query, Fragment) -->
-% zos_path(Path), query(Query), fragment(Fragment).
 
 % magari da riguardare un attimo la ricorsione
 % ci ho riguardato, a me sembra corretta - A. C.
@@ -352,7 +203,10 @@ mailto([], []) --> [].
 news(Host) --> host(Host).
 news([]) --> [].
 
+% fine definizione sotto-grammatiche
+
 % definizione di predicati di supporto
+
 tel_fax(Tel_Fax) :- Tel_Fax = "tel".
 tel_fax(Tel_Fax) :- Tel_Fax = "fax".
 
@@ -379,37 +233,6 @@ id8_2([H | T]) --> [H], {controllo_alfa(H)}, base2(T, controllo_alfanum).
 
 %fine implementazione semplificata!
 
-% VECCHIA IMPLEMENTAZIONE!!!
-/*
-digit([H | []]) --> [H], {controllo_digit(H)}.
-digit([H | T]) --> [H], {controllo_digit(H)}, digit(T).
-
-caratteri([H | []]) --> [H].
-caratteri([H | T]) --> [H], caratteri(T).
-
-caratteri_no_hashtag([H | []]) --> [H], {H \= '#' }.
-caratteri_no_hashtag([H | T]) --> [H], {H \= '#'}, caratteri_no_hashtag(T).
-
-identificatore([H | []]) --> [H],{ controllo_carattere(H)}.
-identificatore([H | T]) --> [H], { controllo_carattere(H)}, identificatore(T).
-
-identificatore_host([H | []]) --> [H], {H \= '.', controllo_carattere(H)}.
-identificatore_host([H | T]) --> [H], {H \= '.', controllo_carattere(H)},
-identificatore_host(T).
-
-id44([H | T]) --> [H], {controllo_alfa(H)},
-id44_recursive(T).
-
-id44_recursive([H | []]) --> [H], {controllo_alfanum(H)}.
-id44_recursive([H | T]) --> [H], {controllo_alfanum(H)}, id44_recursive(T).
-id44_recursive(['.' | T]) --> ['.'], id44_recursive(T).
-
-id8([H | T]) --> [H], {controllo_alfa(H)},
-id8_recursive(T).
-
-id8_recursive([H | []]) --> [H], {controllo_alfanum(H)}.
-id8_recursive([H | T]) --> [H], {controllo_alfanum(H)}, id8_recursive(T).
-*/
 indirizzo_ip(Ip) --> terzina(NNN1), {length(NNN1, 3)}, ['.'],
 terzina(NNN2), {length(NNN2, 3)}, ['.'],
 terzina(NNN3), {length(NNN3, 3)}, ['.'],
@@ -470,5 +293,6 @@ Carattere \= ':'.
 controllo_ident_host(C) :-
 C \= '.',
 controllo_carattere(C).
+
 % fine definizione di predicati di supporto
 %!  %end of file -- progetto.pl
