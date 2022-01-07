c198621bbf062afe22c29fc88bfeac32cb0ee5a6
robalb <laptoplinux1@notamail>
2022-01-08 00:18:36 +0100

    added 80 cols length test

--- tests/tests_formatting.py
+++ tests/tests_formatting.py
@@ -1,9 +1,24 @@
 import unittest
+import os
 
 __unittest = True
 
 class Tests(unittest.TestCase):
     """Formatting and indentation tests
     """
-    def test_80_columns(self):
-        self.assertTrue(False)
+    def test_80_cols_LISP(self):
+      dirname = os.path.dirname(__file__)
+      filename = os.path.join(dirname, '../Lisp/uri-parse.lisp')
+      self.columns_check(filename)
+
+    def test_80_cols_PROLOG(self):
+      dirname = os.path.dirname(__file__)
+      filename = os.path.join(dirname, '../Prolog/uri-parse.prolog')
+      self.columns_check(filename)
+
+    def columns_check(self, path):
+      """Check that a file respect the 80 columns limit"""
+      with open(path, 'r') as f:
+        for line in f:
+          if len(line) > 80:
+            self.fail(f"this line is too long: {line}")
